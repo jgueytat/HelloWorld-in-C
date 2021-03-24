63255b3fe18f4845aba211be247df41eddd32b6c
Julien Gueytat <contact@jgueytat.fr>
2021-03-24 07:52:22 +0000

    Finding the missing breakpoint issue

--- main.cpp
+++ main.cpp
@@ -2,7 +2,12 @@
 
 int main()
 {
+    do {
+        std::cout << "Press a key to continue...";
+    } while (std::cin.get() != '\n');
+
     std::cout << "Hello Foo !" << std::endl;
     std::cout << "Hello Bar !" << std::endl;
+    
     return 0;
 }
