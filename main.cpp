5c021c0a02af33cef53cf92a737ecde84714706f
Julien Gueytat <contact@jgueytat.fr>
2021-11-29 11:40:04 +0000

    Try bugs 260 and 262

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
+#include <cstdio>
 #include <iostream>
 
 int main()
 {
+    std::cout << "Press enter...";
+    std::getchar();
+
     std::cout << "Hello Foo !" << std::endl;
     std::cout << "Hello Bar !" << std::endl;
     
