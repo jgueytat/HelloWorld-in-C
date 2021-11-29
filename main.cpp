baeff31443c1de51f73a72cf1f45705be2228a83
Julien Gueytat <contact@jgueytat.fr>
2021-11-29 11:05:17 +0100

    Give a try on Gitpod

--- main.cpp
+++ main.cpp
@@ -3,9 +3,6 @@
 
 int main()
 {
-    std::cout << "Press enter to continue...";
-    std::getchar();
-
     std::cout << "Hello Foo !" << std::endl;
     std::cout << "Hello Bar !" << std::endl;
     
